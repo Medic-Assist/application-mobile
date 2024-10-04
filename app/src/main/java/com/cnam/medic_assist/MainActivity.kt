b330e14ef895a97ab428e10281c0ae8a715b0435
LudoPilot <e.berardfr@gmail.com>
2024-10-04 14:41:52 +0200

    Ajout des fragments

--- app/src/main/java/com/cnam/medic_assist/MainActivity.kt
+++ app/src/main/java/com/cnam/medic_assist/MainActivity.kt
@@ -9,7 +9,21 @@ import com.ale.rainbowsdk.RainbowSdk
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.cnam.medic_assist.R
 
+// Importation des fragments de la navbar
+import androidx.fragment.app.Fragment
+import com.cnam.medic_assist.ui.fragments.HomeFragment
+import com.cnam.medic_assist.ui.fragments.SuiviFragment
+import com.cnam.medic_assist.ui.fragments.GpsFragment
+import com.cnam.medic_assist.ui.fragments.ProfilFragment
+
 class MainActivity : AppCompatActivity() {
+
+
+    // Ajout des fragments
+    private val homeFragment = HomeFragment()
+    private val suiviFragment = SuiviFragment()
+    private val gpsFragment = GpsFragment()
+    private val profilFragment = ProfilFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,23 +37,28 @@ class MainActivity : AppCompatActivity() {
             when (item.itemId) {
                 R.id.navigation_home -> {
                     // Gérer la navigation home
+                    openFragment(homeFragment)
                     true
                 }
                 R.id.navigation_suivi -> {
                     // Gérer la navigation suivi
+                    openFragment(suiviFragment)
                     true
                 }
                 R.id.navigation_gps -> {
                     // Gérer la navigation GPS
+                    openFragment(gpsFragment)
                     true
                 }
                 R.id.navigation_profil -> {
                     // Gérer la navigation profil
+                    openFragment(profilFragment)
                     true
                 }
                 else -> false
             }
         }
+
 
         class RainbowApplication : Application() {
 
@@ -53,5 +72,11 @@ class MainActivity : AppCompatActivity() {
                 )
             }
         }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
