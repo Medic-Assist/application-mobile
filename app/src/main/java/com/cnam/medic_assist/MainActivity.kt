160c91bb31c652ade735cad910bcf72ad99ac031
Maxime BLAESS <blaessmaxime@gmail.com>
2024-11-15 16:29:24 +0100

    Correction bug

--- app/src/main/java/com/cnam/medic_assist/MainActivity.kt
+++ app/src/main/java/com/cnam/medic_assist/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cnam.medic_assist
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.os.Bundle
@@ -26,6 +27,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonMap = findViewById<Button>(R.id.button_map)
+        buttonMap.setOnClickListener {
+            startActivity(Intent(this, MapActivity::class.java))
+        }
+
+        val buttonDirections = findViewById<Button>(R.id.button_directions)
+        buttonDirections.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            intent.action = "GET_DIRECTIONS"
+            // Définir la destination à la cathédrale de Strasbourg
+            intent.putExtra("DESTINATION", "Cathédrale de Strasbourg")
+            startActivity(intent)
+        }
+
+        val buttonSearchMap = findViewById<Button>(R.id.button_searchMap)
+        buttonSearchMap.setOnClickListener {
+            val destination = findViewById<EditText>(R.id.edit_text_destination).text.toString()
+            if (destination.isNotBlank()) {
+                val intent = Intent(this, MapActivity::class.java)
+                intent.action = "GET_DIRECTIONS"
+                intent.putExtra("DESTINATION", destination)
+                startActivity(intent)
+            }
+        }
+
 
         // Initialiser le client de localisation
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
