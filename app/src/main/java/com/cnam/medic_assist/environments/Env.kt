14810c90cfde77ba54ce2ee46c49cbcdad65009c
LudoPilot <e.berardfr@gmail.com>
2024-11-27 23:09:16 +0100

    Ajout du Env.kt au .gitignore

--- app/src/main/java/com/cnam/medic_assist/environments/Env.kt
+++
@@ -1,7 +0,0 @@
-package com.cnam.medic_assist.environments
-
-object Env {
-    const val BASE_URL = "http://{adresseIPVA}/api/"
-    const val APPLICATION_ID = "1bb522e02a2f11ef8d4b252edcd30f84"
-    const val APPLICATION_SECRET = "mCv1Xce3qhSJTcTsTL6HAdu97ivYcdeH2owmXWLCB9kFE9OUugB5ZFMHpgiK1o7c"
-}
