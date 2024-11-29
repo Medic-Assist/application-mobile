d83cb6556eebe6c8c0f9851a2d87823cb5b96ac6
Dinholu <alizeehett@hotmail.fr>
2024-11-29 14:53:44 +0100

    fix .env

--- app/src/main/java/com/cnam/medic_assist/environments/.Env.kt
+++
@@ -1,7 +0,0 @@
-package com.cnam.medic_assist.environments
-
-object Env {
-    const val BASE_URL = "http://192.168.0.23:3000/api/"
-    const val APPLICATION_ID = "445d6480aca411ef8a10732a375c08b8"
-    const val APPLICATION_SECRET = "2TVurwLxCNPQOd2AAK3hZX8GTzr4FNTpAzrrOPFWUEBzZXTUOUoK4mqoteIeg2sp"
-}
