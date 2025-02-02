87416aa61665d8dc86046623f48af206ef82e374
Baptiste <baptiste.bdry@gmail.com>
2025-02-02 23:05:23 +0100

    Ajout de la récupération et de l'ajout d'un contact rainbow
    Ajout du contact par invitation par email

--- app/src/main/java/com/cnam/medic_assist/ui/fragments/MesProchesFragment.kt
+++ app/src/main/java/com/cnam/medic_assist/ui/fragments/MesProchesFragment.kt
@@ -1,41 +1,69 @@
+// MesProchesFragment.kt
 package com.cnam.medic_assist.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cnam.medic_assist.R
+import com.cnam.medic_assist.utils.ContactManager
+import com.cnam.medic_assist.utils.ContactsAdapter
 
 class MesProchesFragment : Fragment() {
+
+    private lateinit var contactsRecyclerView: RecyclerView
+    private lateinit var contactsAdapter: ContactsAdapter
+    private lateinit var inputEmail: EditText
+    private val contactManager = ContactManager()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // étendre la mise en page pour ce fragment (mes proches)
         return inflater.inflate(R.layout.fragment_mes_proches, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val btnBackToProfile: Button = view.findViewById(R.id.btn_back_to_profile)
+        contactsRecyclerView = view.findViewById(R.id.contacts_recycler_view)
+        contactsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        contactsAdapter = ContactsAdapter(emptyList())
+        contactsRecyclerView.adapter = contactsAdapter
+        inputEmail = view.findViewById(R.id.input_email)
+
+        val btnBackToProfile = view.findViewById<View>(R.id.btn_back_to_profile)
         btnBackToProfile.setOnClickListener {
-            // Retourner au ProfilFragment
             parentFragmentManager.popBackStack()
         }
 
-        val btnModifier: Button = view.findViewById(R.id.btn_modifier)
-        val btnEnregistrer: Button = view.findViewById(R.id.btn_enregistrer)
+        val btnModifier = view.findViewById<View>(R.id.btn_modifier)
+        val btnEnregistrer = view.findViewById<View>(R.id.btn_enregistrer)
 
         btnModifier.setOnClickListener {
-            // TODO : logique de modification
+            Toast.makeText(requireContext(), "Modification en cours", Toast.LENGTH_SHORT).show()
         }
 
         btnEnregistrer.setOnClickListener {
-            // TODO : logique pour enregister les modifs
+            val email = inputEmail.text.toString().trim()
+            if (email.isEmpty()) {
+                Toast.makeText(requireContext(), "Veuillez entrer une adresse e-mail", Toast.LENGTH_SHORT).show()
+            } else {
+                contactManager.inviteUserByEmail(email, requireContext())
+            }
+        }
+
+        loadContacts()
+    }
+
+    private fun loadContacts() {
+        contactManager.loadContacts { contacts ->
+            contactsAdapter.updateContacts(contacts)
         }
     }
 }
